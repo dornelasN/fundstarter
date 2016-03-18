8d69a990da45d505ffc3f58ea4bfaad02d586964
Ubuntu <ubuntu@ip-172-31-47-56.us-west-2.compute.internal>
2016-03-18 00:11:23 +0000

    Read File Async

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 var http = require('http');
 var fs = require('fs');
+
 var requestListener = function (req, res) {
   res.writeHead(200);
+
   fs.readfile('index.html', 'utf8', function (err, data){
         if(err){
         return console.log(err);
         }
+	console.log(data);
         res.end(data);
-  })
+  });
 }
 
 var server = http.createServer(requestListener);
-server.listen(PORT);
+server.listen(PORT, function(){
+	console.log("Server listening");
+});
